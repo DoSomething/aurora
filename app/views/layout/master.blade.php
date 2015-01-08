613224bc7589aaaffbb2c650d6fdf0e6f0603c41
Andrea Gaither <angaither@gmail.com>
2015-01-08 11:49:20 -0500

    Todo, gulp, grunt.

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <title>Aurora</title>
   <!-- include bootstrap -->
+  <!-- @TODO include this with gulp, grunt, etc. -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
