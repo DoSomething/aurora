b3296a73b6b50ae3f3267dffe5be05bd8814a5be
Andrea Gaither <angaither@gmail.com>
2015-01-15 09:47:00 -0500

    Added user show template.

---
+++ app/views/users/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.master')
+
+@section('main_content')
+
+ @foreach($user as $key => $field)
+  @if (!in_array($key, ['created_at', 'updated_at', 'campaigns']))
+     <dt><strong>{{ $key }}</strong> </dt>
+     <dl> {{ $field }} </dl>
+   @endif
+
+  @if ($key == 'campaigns')
+  @foreach($field as $campaigns)
+  {{var_dump($campaigns)}}
+  @endforeach
+  @endif
+ @endforeach
+
+@stop
