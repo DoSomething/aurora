7fa1b9359afec0d78b03dd6fb4df607bb0b50531
Andrea Gaither <angaither@gmail.com>
2015-01-15 16:38:37 -0500

    Created users table migration.

---
+++ app/database/migrations/2015_01_15_213047_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function(Blueprint $table) {
+      $table->increments('id');
+      $table->string('_id')->unique->nullable();
+      $table->string('remember_token', 100)->nullable();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('users');
+  }
+
+}
