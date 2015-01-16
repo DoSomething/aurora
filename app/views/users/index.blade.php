065ccd935aa23c7453956164eac1f10958d42521
Andrea Gaither <angaither@gmail.com>
2015-01-16 10:43:52 -0800

    Updated users_index view file to use dynamic content.

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -4,33 +4,30 @@
 
 @include('users.partials.search')
 
-<table class= "table table-striped table-bordered">
-  <thead>
-    <tr>
-      <th>_id</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Email</th>
-      <th>Phone</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-    <!--@TODO: input real user content. -->
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>gmail@mdo</td>
-      <td>55555555</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>gmail@fat</td>
-      <td>4444444</td>
-    </tr>
-  </tbody>
-</table>
+@if ($users)
+  <table class= "table table-striped table-bordered">
+    <thead>
+      <tr>
+        <th>_id</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($users as $user)
+        <tr>
+          <td> {{ link_to_route('users.show', $user['_id'], array($user['_id'])) }}</td>
+          <td> {{ $user['first_name'] or '' }}</td>
+          <td> {{ $user['last_name'] or '' }}</td>
+          <td> {{ $user['email']  or '' }}</td>
+          <td> {{ $user['mobile'] or '' }}</td>
+
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+@endif
 
 @stop
