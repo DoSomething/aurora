dff3e2a8bfb629db92aacc0c0b4d98a71b649984
Andrea Gaither <angaither@gmail.com>
2015-01-23 12:58:17 -0500

    Added admin@ds.org to db, as an admin.

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,5 +6,7 @@ class UserTableSeeder extends Seeder {
    * Seeds the users table.
    */
   public function run() {
+    // admin@dosomething.org
+    User::create(['_id' => '54c28b607e7bc2667e8b4588'])->assignRole(1);
   }
 }
