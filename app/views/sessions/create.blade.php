f346c5d238d7c64c1917381581b14966fd109159
Andrea Gaither <angaither@gmail.com>
2015-01-26 15:21:12 -0500

    Add labels, form group div to signin form.

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -6,11 +6,15 @@
 
   {{ Form::open(['route' => 'sessions.store', 'class' => 'form-signin']) }}
     <h2 class="form-signin-heading">Please sign in</h2>
+    <div class="form-group">
+      {{ Form::label('email', 'Email', array('class' => 'sr-only')) }}
+      {{ Form::text('email', NULL, array('class' => 'form-control', 'placeholder' => 'Email Address')) }}
+    </div>
 
-
-    {{ Form::text('email', NULL, array('class' => 'form-control', 'placeholder' => 'Email Address')) }}
-
-    {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
+    <div class="form-group">
+      {{ Form::label('password', 'Password', array('class' => 'sr-only')) }}
+      {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
+    </div>
 
     {{ Form::submit('Sign in', array('class' => 'btn btn-lg btn-primary btn-block')) }}
 
