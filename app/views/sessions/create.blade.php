14c920feeb43222bf88674cda3c90dcd17cb973b
Andrea Gaither <angaither@gmail.com>
2015-01-26 15:14:28 -0500

    Updating form sign-in styles.

--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('main_content')
 
-<div class ="container">
+<div class ="container col-md-6">
 
   {{ Form::open(['route' => 'sessions.store', 'class' => 'form-signin']) }}
     <h2 class="form-signin-heading">Please sign in</h2>
@@ -10,7 +10,7 @@
 
     {{ Form::text('email', NULL, array('class' => 'form-control', 'placeholder' => 'Email Address')) }}
 
-    {{ Form::password('password', NULL, array('class' => 'form-control', 'placeholder' => 'Password')) }}
+    {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
 
     {{ Form::submit('Sign in', array('class' => 'btn btn-lg btn-primary btn-block')) }}
 
