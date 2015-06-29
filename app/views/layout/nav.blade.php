e0d2247dcf2ab8ba5ca77d949a33bb51ea5ce638
axton21 <axton_n@yahoo.com>
2015-06-29 17:35:17 -0400

    intial login page styling w/ DS-modal

--- app/views/layout/nav.blade.php
+++ app/views/layout/nav.blade.php
@@ -1,26 +1,34 @@
-<nav class="navigation {{'modifier_class'}}">
-  <a class="navigation__logo" href=""><span>DoSomething.org</span></a>
-  <div class="navigation__menu">
-  <ul class="navigation__primary">
-    <li>
-      <h1 id="brand-title">Aurora</h1>
+  <nav class="navigation {{'modifier_class'}}">
+    <a class="navigation__logo" href=""><span>DoSomething.org</span></a>
+    <div class="navigation__menu">
+    <ul class="navigation__primary">
+      <li>
+        <h1 id="brand-title">Aurora</h1>
+      </li>
+    </ul>
+    <ul class="navigation__secondary">
+      <li>
+        <form action="#" method="post">
+          <input class="text-field -search" type="search">
+        </form>
+      </li>
+      <li>
+      @if (Auth::user())
+        <li> {{ link_to_route('keys.index', 'Keys') }} </li>
+        <li> {{ link_to_route('users.index', 'Users') }} </li>
+        <li> {{ link_to_route('logout', 'Logout') }} </li>
+      @else
+        <li>
+           <!-- {{ link_to_route('login', 'Login') }} -->
+
+          <!-- Modal Stuff  -->
+          <a href="#" data-modal-href="#signin-modal" class="signinModal">Login</a>
+          <!-- Modal Stuff -->
+       </li>
+
+      @endif
     </li>
-  </ul>
-  <ul class="navigation__secondary">
-    <li>
-      <form action="#" method="post">
-        <input class="text-field -search" type="search">
-      </form>
-    </li>
-    <li>
-    @if (Auth::user())
-      <li> {{ link_to_route('keys.index', 'Keys') }} </li>
-      <li> {{ link_to_route('users.index', 'Users') }} </li>
-      <li> {{ link_to_route('logout', 'Logout') }} </li>
-    @else
-      <li> {{ link_to_route('login', 'Login') }} </li>
-    @endif
-    </li>
-  </ul>
-  </div>
-</nav>
+    </ul>
+    </div>
+  </nav>
+     @include('sessions.create')
--- app/views/sessions/create.blade.php
+++ app/views/sessions/create.blade.php
@@ -1,24 +1,23 @@
-@extends('layout.master')
-
-@section('main_content')
-
-<div class ="container col-md-6">
-
+<div data-modal id="signin-modal" role="dialog">
+  <h2 class="heading -banner">Please Sign In</h2>
+  @if (Session::has('flash_message'))
+  <div class="messages {{'modifier_class'}}">
+    <div class="flash-message {{ Session::get('flash_message')['class'] }}">
+      <em>{{ Session::get('flash_message')['text'] }}</em>
+    </div>
+  </div>
+  @endif
+  <div class="modal__block">
   {{ Form::open(['route' => 'sessions.store', 'class' => 'form-signin']) }}
-    <h2 class="form-signin-heading">Please sign in</h2>
-    <div class="form-group">
+    <div class="form-item {{ 'modifier_class' }} -padded">
       {{ Form::label('email', 'Email', array('class' => 'sr-only')) }}
-      {{ Form::text('email', NULL, array('class' => 'form-control', 'placeholder' => 'Email Address')) }}
+      {{ Form::text('email', NULL, array('class' => 'text-field', 'placeholder' => 'Email Address')) }}
     </div>
-
-    <div class="form-group">
+    <div class="form-item {{ 'modifier_class' }} -padded">
       {{ Form::label('password', 'Password', array('class' => 'sr-only')) }}
-      {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
+      {{ Form::password('password', array('class' => 'text-field', 'placeholder' => 'Password')) }}
     </div>
-
     {{ Form::submit('Sign in', array('class' => 'button')) }}
-
   {{ Form::close() }}
+  </div>
 </div>
-
-@stop
