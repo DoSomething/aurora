07766541dd673618381f50dfe23454d8b29491a5
axton21 <axton_n@yahoo.com>
2015-07-01 18:15:08 -0400

    removed bootsrap class from root page for now

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,10 +1,5 @@
 @extends('layout.master')
 
 @section('main_content')
-
- <div class="container theme-showcase" role="main">
-    <div class="jumbotron">
-      <h1>You have arrived.</h1>
-    </div>
-  </div>
+       <h1>You have arrived.</h1>
 @stop
