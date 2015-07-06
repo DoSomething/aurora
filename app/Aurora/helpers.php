fc93d5d3f73536e22545fd37ee5bcde64dfdb145
Paul Han <CRABAPPLE@CRABAPPLEs-MacBook-Pro.local>
2015-07-06 14:25:43 -0400

    got rid of keysantizer function

--- app/Aurora/helpers.php
+++ app/Aurora/helpers.php
@@ -8,10 +8,3 @@ window.DSModal.open($("#signin-modal"));
 });
 </script>';
 }
-
-function keySantizer($key){
-$key = str_replace("_"," ", $key);
-$key = preg_replace('/^\s/',"", $key);
-$key = ucfirst(preg_replace('/addr /',"", $key));
-return $key;
-}
