91527fda74253ebd701399a693fba11a6920617b
Paul Han <CRABAPPLE@CRABAPPLEs-MacBook-Pro.local>
2015-07-07 17:05:23 -0400

    got rid of unncessary paritals

--- app/views/keys/create.blade.php
+++ app/views/keys/create.blade.php
@@ -5,8 +5,15 @@
 <div class ="container -padded">
   {{ Form::open(['route' => 'keys.store', 'method' => 'post']) }}
 
-		@include('keys.partials.form')
+		<div class="container__block -half">
+			<h1 class="heading -alpha">Create a New App</h1>
+			  <div class="form-item -padded">
+				  {{ Form::label('App Name', null, ['class' => 'field-label']) }}
 
+			    {{ Form::text('app_name', NULL, ['class' => 'text-field']) }}
+				</div>
+			    {{ Form::submit('Submit', ['class' => 'button']) }}
+		</div>
 
   {{ Form::close() }}
 </div>
--- app/views/keys/index.blade.php
+++ app/views/keys/index.blade.php
@@ -3,11 +3,22 @@
 @section('main_content')
 <div class="container__block">
 	<a href="{{ URL::Route('keys.create') }}"><span class='glyphicon glyphicon-plus'></span>New App</a>
-
+</div>
+<div class="container__block">
 	<h2> Listing of all Northstar api keys. </h2>
-
 	@if ($keys)
-		@include('keys.partials.api-keys')
+		<ul class="gallery -duo">
+		  @foreach($keys as $key)
+		    <li>
+		      <article class="figure -left">
+		          <div class = "well" >
+		           <dt><strong> App ID: </strong> {{ $key['app_id'] }} </dt>
+		           <dt><strong> API Key: </strong>{{ $key['api_key'] }} </dt>
+		          </div>
+		      </article>
+		    </li>
+		  @endforeach
+		</ul>
 	@endif
 	
 </div>
