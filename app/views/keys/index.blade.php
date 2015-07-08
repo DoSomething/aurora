2e0e1653eeb9ebffd8dab223bb643612ac3181e9
Paul Han <CRABAPPLE@CRABAPPLEs-MacBook-Pro.local>
2015-07-08 10:45:16 -0400

    use route instead of URL facade

--- app/views/keys/index.blade.php
+++ app/views/keys/index.blade.php
@@ -3,7 +3,7 @@
 @section('main_content')
 <div class="container -padded">
 	<div class="container__block">
-		<a href="{{ URL::Route('keys.create') }}">New App</a>
+		<a href="{{ route('keys.create') }}">New App</a>
 	</div>
 		<h2> Listing of all Northstar API keys</h2>
 		@if ($keys)
