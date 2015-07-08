8a7228ffe3c1862286e7937c87bef40c6eec09f8
Paul Han <CRABAPPLE@CRABAPPLEs-MacBook-Pro.local>
2015-07-08 10:40:55 -0400

    spacing and padding

--- app/views/keys/index.blade.php
+++ app/views/keys/index.blade.php
@@ -1,24 +1,23 @@
 @extends('layout.master')
 
 @section('main_content')
-<div class="container__block">
-	<a href="{{ URL::Route('keys.create') }}"><span class='glyphicon glyphicon-plus'></span>New App</a>
-</div>
-<div class="container__block">
-	<h2> Listing of all Northstar API keys. </h2>
-	@if ($keys)
-		<ul class="gallery -duo">
-		  @foreach($keys as $key)
-		    <li>
-		      <article class="figure -left">
-		          <div class = "well" >
-		           <dt><strong> App ID: </strong> {{ $key['app_id'] }} </dt>
-		           <dt><strong> API Key: </strong>{{ $key['api_key'] }} </dt>
-		          </div>
-		      </article>
-		    </li>
-		  @endforeach
-		</ul>
-	@endif
+<div class="container -padded">
+	<div class="container__block">
+		<a href="{{ URL::Route('keys.create') }}">New App</a>
+	</div>
+		<h2> Listing of all Northstar API keys</h2>
+		@if ($keys)
+			<ul class="gallery -duo">
+			  @foreach($keys as $key)
+			    <li>
+			      <article class="figure -left">
+	          	<dt><strong> App ID: </strong> {{ $key['app_id'] }} </dt>
+	          	<dt><strong> API Key: </strong>{{ $key['api_key'] }} </dt>
+			      </article>
+			    </li>
+			  @endforeach
+			</ul>
+		@endif
+	</div>
 </div>
 @stop
