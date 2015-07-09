305b49c27fe129a15b1e0e0fe6a8800d18cef7f4
Paul Han <CRABAPPLE@CRABAPPLEs-MacBook-Pro.local>
2015-07-09 17:18:45 -0400

    moved campaigns to its own section

--- app/views/users/partials/details.blade.php
+++ app/views/users/partials/details.blade.php
@@ -14,22 +14,23 @@
 			</dl>
 		</article>
 	</li>
-
-	<!-- not actually displaying campaigns, need to make call to api -->
-	<li>
-	  <article class="figure -medium">
-	  <h3>Campaigns</h3>
-	      @forelse ($campaigns as $campaign)
-          <div class="figure__media">
-						<img src= "{{ $campaign['cover_image']['default']['uri'] }}">
-					</div>
-					<div class="figure__body">
-						<b>{{ $campaign['title'] }}</b>
-					</div>
-				@empty
-					<h3>This user has no campaigns</h3>	
-      	@endforelse
-  	</article>
-	</li>
 </ul>
 
+
+<h3>Campaigns</h3>
+<ul class="gallery -triad " data-show-more="true">
+	@forelse ($campaigns as $campaign)
+		<li>
+	    <div class="figure">
+	      <div class="figure__media">
+					<img src= "{{ $campaign['cover_image']['default']['uri'] }}">
+				</div>
+				<div class="figure__body">
+					<b>{{ $campaign['title'] }}</b>
+				</div>
+			</div>
+		</li>
+	@empty
+		<h3>This user has no campaigns</h3>	
+	@endforelse
+</ul>
