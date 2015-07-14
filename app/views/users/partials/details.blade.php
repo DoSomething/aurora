bbd6894034fa1ddccfe8c8ada0697e0b57460a01
axton21 <axton_n@yahoo.com>
2015-07-14 17:11:45 -0400

    fix user detail view for not showing empty attributes

--- app/views/users/partials/details.blade.php
+++ app/views/users/partials/details.blade.php
@@ -4,14 +4,17 @@
 			<h3>Account Info</h3>
 			<dl class="profile-settings">
 				<dt>Id:</dt><dd>{{ $user['_id'] or '' }}</dd>
-				<dt>Drupal Id:</dt><dd>{{ $user['drupal_id'] or '' }}</dd>
-				<dt>First Name:</dt><dd>{{ $user['first_name'] or '' }}</dd>
-				<dt>Last Name:</dt><dd>{{ $user['last_name'] or '' }}</dd>
-				<dt>Email:</dt><dd>{{ $user['email'] or '' }}</dd>
-				<dt>Mobile:</dt><dd>{{ $user['mobile'] or '' }}</dd>
-				<dt>Birthday:</dt><dd>{{ $user['birthdate'] or '' }}</dd>
-				<dt>Address:</dt><dd>{{ $user['addr_street1'] or ''}} {{ $user['addr_street2'] or ''}} {{ $user['add_city'] or ''}} {{ $user['addr_state'] or ''}} {{ $user['addr_zip'] or ''}}</dd>
-				<dt>Country:</dt><dd>{{ $user['country'] or '' }}</dd>
+				{{ isset($user['drupal_id']) ? ('<dt>Drupal Id:</dt><dd>'.$user['drupal_id']) : "" }}
+				{{ isset($user['firt_name']) ? ('<dt>First Name:</dt><dd>'.$user['firt_name']) : "" }}
+				{{ isset($user['last_name']) ? ('<dt>Last Name:</dt><dd>'.$user['last_name']) : "" }}
+				{{ isset($user['email']) ? ('<dt>Email:</dt><dd>'.$user['email']) : "" }}
+				{{ isset($user['mobile']) ? ('<dt>Mobile:</dt><dd>'.$user['mobile']) : "" }}
+				{{ isset($user['birthdate']) ? ('<dt>Birthday:</dt><dd>'.$user['birthdate']) : "" }}
+				{{ isset($user['drupal_id']) ? ('<dt>Address:</dt><dd>'.$user['drupal_id']) : "" }}
+				@if (isset($user['addr_street1']) || isset($user['addr_street2']) || isset($user['add_city']) || isset($user['addr_state']) || isset($user['addr_zip']) )
+					<dt>Address:</dt><dd>{{ $user['addr_street1'] or ''}} {{ $user['addr_street2'] or ''}} {{ $user['add_city'] or ''}} {{ $user['addr_state'] or ''}} {{ $user['addr_zip'] or ''}}</dd>
+				@endif
+				{{ isset($user['country']) ? ('<dt>Country:</dt><dd>'.$user['country']) : "" }}
 			</dl>
 		</article>
 	</li>
