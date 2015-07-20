a1ecea0a3eb59ef153df6ebaa80d16ac163a8c26
axton21 <axton_n@yahoo.com>
2015-07-20 17:38:08 -0400

    using partial for login form

--- app/views/sessions/signin-modal.blade.php
+++ app/views/sessions/signin-modal.blade.php
@@ -6,16 +6,6 @@
     </div>
   @endif
   <div class="modal__block">
-  {{ Form::open(['route' => 'sessions.store', 'class' => 'form-signin']) }}
-    <div class="form-item -padded">
-      {{ Form::label('email', 'Email', array('class' => 'sr-only')) }}
-      {{ Form::text('email', NULL, array('class' => 'text-field', 'placeholder' => 'Email Address')) }}
-    </div>
-    <div class="form-item -padded">
-      {{ Form::label('password', 'Password', array('class' => 'sr-only')) }}
-      {{ Form::password('password', array('class' => 'text-field', 'placeholder' => 'Password')) }}
-    </div>
-    {{ Form::submit('Sign in', array('class' => 'button')) }}
-  {{ Form::close() }}
+    @include('sessions.partials.login-form')
   </div>
 </div>
