a1ac1a2e3ce4609ead35d4e3f135aa71de8b882e
Paul Han <CRABAPPLE@CRABAPPLEs-MacBook-Pro.local>
2015-07-20 11:57:44 -0400

    changed jquery version

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="{{ asset('assets/vendor/neue/neue.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/vendor/modal/modal.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/custom-neue.css') }}">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="{{ asset('/assets/vendor/neue/modernizr.js') }}"></script>
 </head>
 
