179ddf9c73c1b587fded9db47c110fe8c1aaa408
axton21 <axton_n@yahoo.com>
2015-07-19 21:17:56 -0400

    styled admin index page

--- app/views/users/admin-index.blade.php
+++ app/views/users/admin-index.blade.php
@@ -3,17 +3,28 @@
 @section('main_content')
 
 @include('layout.header', ["header" => "Admin Index", "subtitle" => "Listing of all admins"])
-
-<h1 class="heading -banner"><span>Account Info</span></h1>
-<div class="container -padded">
-  <div class="wrapper">
-		@forelse($admins as $admin)
-			{{$admin['first_name'] or '' }}
-			{{$admin['last_name'] or '' }}
-			{{$admin['email'] or '' }}
-		@empty
-		No Admin
-		@endforelse
-	</div>
-</div>
+<ul class="gallery -duo">
+  @forelse($admins as $admin)
+    <li>
+      <article class="figure -left admin-box">
+        <dl class="profile-settings">
+          <dt>Id:</dt><dd>{{{ $admin['_id'] }}}</dd>
+          {{ isset($admin['drupal_id']) ? ('<dt>Drupal Id:</dt><dd>' . e($admin['drupal_id']) . '</dd>') : "" }}
+          {{ isset($admin['first_name']) ? ('<dt>First Name:</dt><dd>' . e($admin['first_name']) . '</dd>') : "" }}
+          {{ isset($admin['last_name']) ? ('<dt>Last Name:</dt><dd>' . e($admin['last_name']) . '</dd>') : "" }}
+          {{ isset($admin['email']) ? ('<dt>Email:</dt><dd>' . e($admin['email']) . '</dd>') : "" }}
+          {{ isset($admin['mobile']) ? ('<dt>Mobile:</dt><dd>' . e($admin['mobile']) . '</dd>') : "" }}
+          {{ isset($admin['mobile']) ? ('<dt>Mobile:</dt><dd>' . e(sanitizePhoneNumber($admin['mobile'], isset($admin['country']) ? $admin['country'] : "")) .'</dd>')  : '' }}</td>
+          {{ isset($admin['birthdate']) ? ('<dt>Birthday:</dt><dd>' . e($admin['birthdate']) . '</dd>') : "" }}
+          @if (isset($admin['addr_street1']) || isset($admin['addr_street2']) || isset($admin['add_city']) || isset($admin['addr_state']) || isset($admin['addr_zip']) )
+            <dt>Address:</dt><dd>{{{ $admin['addr_street1'] or '' }}} {{{ $admin['addr_street2'] or '' }}} {{{ $admin['add_city'] or '' }}} {{{ $admin['addr_state'] or '' }}} {{{ $admin['addr_zip'] or '' }}}</dd>
+          @endif
+          {{ isset($admin['country']) ? ('<dt>Country:</dt><dd>' . e($admin['country']) . '</dd>') : "" }}
+        </dl>
+      </article>
+    </li>
+  @empty
+    There is no admin admin
+  @endforelse
+</ul>
 @stop
