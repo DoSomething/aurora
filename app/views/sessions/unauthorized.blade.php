46cebfe4b6ce6455300c3d7eb183e77a1be0c67e
Paul Han <CRABAPPLE@CRABAPPLEs-MacBook-Pro.local>
2015-07-21 10:37:33 -0400

    centered gifs

--- app/views/sessions/unauthorized.blade.php
+++ app/views/sessions/unauthorized.blade.php
@@ -2,12 +2,11 @@
 
 @section('main_content')
 
-@include('layout.header', ['header' => 'Permission denied', 'subtitle' => ''])
+@include('layout.header', ['header' => 'Permission denied', 'subtitle' => 'You need admin privileges to view this page!'])
 	<div class="container -padded">
 		<div class="wrapper">
 			<div class="container__block">
-				<h2>You need admin privileges to view this page!</h2>
-				<iframe src="//giphy.com/embed/{{ $gif }}" width="480" height="384" frameBorder="0" style="max-width: 100%" class="giphy-embed" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+				<center><iframe src="//giphy.com/embed/{{ $gif }}" width="480" height="384" frameBorder="0" style="max-width: 100%" class="giphy-embed" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe><center>
 			</div>
 		</div>
 	</div>
