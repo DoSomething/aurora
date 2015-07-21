29df3afbd9d011686d5b34577c6bf48be881e485
axton21 <axton_n@yahoo.com>
2015-07-21 17:27:01 -0400

    fix empty array in mc_profile

--- app/views/users/partials/mobile-commons.blade.php
+++ app/views/users/partials/mobile-commons.blade.php
@@ -7,9 +7,9 @@
 				<dl class="profile-settings">
 					<dt>Mobile Commons Id:</dt><dd>{{{ $mc_profile['@attributes']['id'] or '' }}}</dd>
 					<dt>Status:</dt><dd>{{{ $mc_profile['status'] or '' }}}</dd>
-					{ isset($mc_profile['created_at']) ? ('<dt>Signed Up On:</dt><dd>' . e(time_formatter($mc_profile['created_at']))) : "" }
-					{ isset($mc_profile['opted_out_at']) ? ('<dt>Opted Out On:</dt><dd>' . e(time_formatter($mc_profile['opted_out_at']))) : "" }
-					{ isset($mc_profile['opted_out_source']) ? ('<dt>Opted Out Source:</dt><dd>' . e($mc_profile['opted_out_source'])) : "" }
+					{{ isset($mc_profile['created_at']) ? ('<dt>Signed Up On:</dt><dd>' . e(time_formatter($mc_profile['created_at']))) : "" }}
+					{{ !empty($mc_profile['opted_out_at']) ? ('<dt>Opted Out On:</dt><dd>' . e(time_formatter($mc_profile['opted_out_at']))) : "" }}
+					{{ !empty($mc_profile['opted_out_source']) ? ('<dt>Opted Out Source:</dt><dd>' . e($mc_profile['opted_out_source'])) : "" }}
 					<dt>{{ link_to("https://secure.mcommons.com/profiles/".e($mc_profile['@attributes']['id']), "View Mobile Commons Profile") }}</dt>
 					<dt><a href="{{{ url('users/' . $user['_id'] . '/mobile-commons-messages') }}}">View Message Backlog</a></dt>
 				</dl>
