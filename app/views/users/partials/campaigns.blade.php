e28bd3d1b41e39958857a291f77922676d652373
axton21 <axton_n@yahoo.com>
2015-07-24 13:51:52 -0400

    change to readable conditional blade if syntax

--- app/views/users/partials/campaigns.blade.php
+++ app/views/users/partials/campaigns.blade.php
@@ -3,7 +3,11 @@
 		<li>
 	    <div class="figure">
 	      <div class="figure__media">
-					{{ isset($campaign['cover_image']['default']['uri']) ? ('<img src=' . e($campaign['cover_image']['default']['uri']) . '>') : '<img src='. (asset('assets/images/campaign-placeholder.png')) .'>' }}
+					@if (isset($campaign['cover_image']['default']['uri']))
+						<img src="{{ e($campaign['cover_image']['default']['uri']) }}" />
+					@else
+						<img src="{{ asset('assets/images/campaign-placeholder.png') }}" style="width:288px;height:288px;" />
+					@endif
 				</div>
 				<div class="figure__body">
 					<b>{{ isset($campaign['title']) ? e($campaign['title']) : "Not an actual campaign" }}</b>
