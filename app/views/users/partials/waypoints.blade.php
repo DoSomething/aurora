71869e45d6604ad8a1ca51b1e51c384a5a99934e
axton21 <axton_n@yahoo.com>
2015-07-25 01:29:03 -0400

    finishing up

--- app/views/users/partials/waypoints.blade.php
+++ app/views/users/partials/waypoints.blade.php
@@ -1,4 +1,4 @@
-<ul class = "waypoints pagination-buttons" id="pagination-box">
+<ul class = "waypoints -primary" id="pagination-buttons">
   @if ($data['current_page'] > 1)
     <?php $prev = $data['current_page'] - 1 ?>
     <li> {{ link_to_route('users.index', 'First', array('page=1')) }} </li>
@@ -8,11 +8,11 @@
   <?php $lastLeft = ($data['current_page'] - 5 >= 1) ? $data['current_page'] - 5 : 1 ?>
   <?php $lastRight = ($data['current_page'] + 4 <= $data['last_page']) ? $data['current_page'] + 4 : $data['last_page'] ?>
   <?php $lastRight = $lastRight < 10 && $data['last_page'] > 10 ? 10 : $lastRight ?>
-  <?php $lastLeft = $lastLeft > 10 && $data['last_page'] > 10 ? $lastRight-9 : $lastLeft ?>
+  <?php $lastLeft = $lastLeft > 10 && $data['last_page'] > 10 ? $lastRight - 9 : $lastLeft ?>
 
   @foreach(range($lastLeft, $lastRight) as $page)
     @if($data['current_page'] == $page)
-      <li>{{ $data['current_page'] }}</li>
+      <li class="is-active">{{ $data['current_page'] }}</li>
     @else
       <li>{{ link_to_route('users.index', $page , array('page='. $page)) }}</li>
     @endif
@@ -23,5 +23,4 @@
     <li> {{ link_to_route('users.index', 'Next', array('page=' . $next)) }} </li>
     <li> {{ link_to_route('users.index', 'Last', array('page='. $data['last_page'])) }} </li>
   @endif
-
 </ul>
