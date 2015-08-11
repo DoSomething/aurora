7505964d39d48ebb8e6fa1cf750696fe56e041c9
axton21 <axton_n@yahoo.com>
2015-08-11 17:39:46 -0400

    indented users index

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -10,35 +10,36 @@
   <div class="wrapper">
 
     @include('users.partials.search')
-    <a href="#" id="advanced-search-link">Advanced Search</a>
+    <a href="#" id="advanced-search-link">Advance Search Users </a>
 
     <div id="advanced-search-container" class="container__block -narrow">
       {{ Form::open(array('url' => '/advanced-search')) }}
-        <label class="option -radio">
-          {{ Form::radio('source', 'agg', false) }}
-          <span class="option__indicator"></span>
-          {{ Form::label('Agg') }}
-        </label>
-        <label class="option -radio">
-          {{ Form::radio('source', 'cgg', false) }}
-          <span class="option__indicator"></span>
-          {{ Form::label('Cgg') }}
-        </label>
-        <label class="option -radio">
-          {{ Form::radio('source', 'drupal', false) }}
-          <span class="option__indicator"></span>
-          {{ Form::label('Drupal') }}
-        </label>
-        <label class="option -radio">
-          {{ Form::radio('source', 'services', false) }}
-          <span class="option__indicator"></span>
-          {{ Form::label('Services') }}
-        </label>
-        <div class="form-item -inline">
-          {{ Form::label('country', 'Country', ['class' => 'field-label']) }}
-          {{ Form::text('country', NULL, ['class' => 'text-field']) }}
-        </div>
-        {{ Form::submit('Search', ['class' => 'button'])}}
+      {{ Form::label('source', 'Source', ['class' => 'field-label']) }}
+      <label class="option -radio">
+        {{ Form::radio('source', 'agg', false) }}
+        <span class="option__indicator"></span>
+        {{ Form::label('Agg') }}
+      </label>
+      <label class="option -radio">
+        {{ Form::radio('source', 'cgg', false) }}
+        <span class="option__indicator"></span>
+        {{ Form::label('Cgg') }}
+      </label>
+      <label class="option -radio">
+        {{ Form::radio('source', 'drupal', false) }}
+        <span class="option__indicator"></span>
+        {{ Form::label('Drupal') }}
+      </label>
+      <label class="option -radio">
+        {{ Form::radio('source', 'services', false) }}
+        <span class="option__indicator"></span>
+        {{ Form::label('Services') }}
+      </label>
+      <div class="form-item -inline">
+        {{ Form::label('country', 'Country', ['class' => 'field-label']) }}
+        {{ Form::text('country', NULL, ['class' => 'text-field']) }}
+      </div>
+      {{ Form::submit('Search', ['class' => 'button'])}}
       {{ Form::close() }}
     </div>
     @if ($users)
