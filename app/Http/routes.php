4309908558c8adea9db29bae151611a69b2d98ba
David Furnes <david@dfurnes.com>
2016-01-25 10:24:57 -0500

    Clean up routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,48 +1,27 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+/**
+ * Set routes for the application.
+ *
+ * @var \Illuminate\Routing\Router $router
+ * @see \Northstar\Providers\RouteServiceProvider
+ */
 
-Route::get('/', function () {
+$router->get('/', function () {
     return redirect()->route('users.index');
 });
 
 // Authentication
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+$router->get('auth/login', 'Auth\AuthController@getLogin');
+$router->post('auth/login', 'Auth\AuthController@postLogin');
+$router->get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Users
-Route::resource('users', 'UsersController', ['except' => ['create', 'store']]);
+$router->resource('users', 'UsersController', ['except' => ['create', 'store']]);
+$router->get('search', ['as' => 'user.search', 'uses' => 'UsersController@search']);
 
 // Aurora Users
-Route::resource('aurora-users', 'AuroraUsersController', ['only' => ['index', 'edit', 'update']]);
-
-// Delete Northstar User
-Route::delete('northstar-user-delete/{user}', ['as' => 'northstar.delete', 'uses' => 'UsersController@deleteNorthstarUser']);
-
-// Display edit form with merged users
-Route::get('merge', ['as' => 'users.merge', 'uses' => 'UsersController@mergedForm']);
-
-// Delete other users that were not selected
-Route::post('merge', ['as' => 'users.merge-and-delete', 'uses' => 'UsersController@deleteUnmergedUsers']);
-
-// Create admins.
-Route::post('role/{user}', ['as' => 'role.create', 'uses' => 'UsersController@roleCreate']);
+$router->resource('aurora-users', 'AuroraUsersController', ['only' => ['index', 'edit', 'update']]);
 
 // Key
-Route::resource('keys', 'KeyController');
-
-// Search
-Route::get('search', ['as' => 'users.search', 'uses' => 'UsersController@search', 'before' => 'auth']);
-
-// Advanced Search
-Route::get('/advanced-search', 'UsersController@advancedSearch');
+$router->resource('keys', 'KeyController');
