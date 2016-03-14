26dab6fc9c495b446a8685c48d56b1bac65f17c3
David Furnes <david@dfurnes.com>
2016-03-14 13:27:28 -0400

    Re-order fields on profile.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,12 +14,14 @@
                 <h3>Profile</h3>
                 <dt>ID:</dt><dd>{{ $user->id }}</dd>
                 <dt>Drupal ID:</dt><dd>{{ $user->drupal_id or '&mdash;' }}</dd>
-                <dt>Source:</dt><dd>{{ $user->source or '&mdash;' }}</dd>
-                <dt>First Name:</dt><dd>{{ $user->first_name or '&mdash;' }}</dd>
-                <dt>Last Name:</dt><dd>{{ $user->last_name or '&mdash;' }}</dd>
                 <dt>Email:</dt><dd>{{ $user->email or '&mdash;' }}</dd>
                 <dt>Mobile:</dt><dd>{{ $user->prettyMobile('&mdash;') }}</dd>
+                <dt>First Name:</dt><dd>{{ $user->first_name or '&mdash;' }}</dd>
+                <dt>Last Name:</dt><dd>{{ $user->last_name or '&mdash;' }}</dd>
                 <dt>Birthdate:</dt><dd>{{ $user->birthdate or '&mdash;' }}</dd>
+
+
+                <dt>Source:</dt><dd>{{ $user->source or '&mdash;' }}</dd>
 
                 @if (isset($user->addr_street1) || isset($user->addr_street2) || isset($user->addr_city) || isset($user->addr_state) || isset($user->addr_zip) )
                     <dt>Address:</dt><dd>{{ $user->addr_street1 or '' }} {{ $user->addr_street2 or '' }} {{ $user->addr_city or '' }} {{ $user->addr_state or '' }} {{ $user->addr_zip or '' }}</dd>
