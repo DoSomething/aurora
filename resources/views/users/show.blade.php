25f74aa9c451e41ced1f7e2bdf13e8ce1cd3ef20
David Furnes <david@dfurnes.com>
2016-08-03 14:12:04 -0400

    :eyes:

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -76,7 +76,7 @@
                     <dt>Phoenix:</dt><dd>&mdash;</dd>
                 @endif
 
-                <dt>Gladiator:</dt><dd><a href="{{ config('services.gladiator.url') }}/user/{{ $user->id }}">{{ $user->id }}</a></dd>
+                <dt>Gladiator:</dt><dd><a href="{{ config('services.gladiator.url') }}/users/{{ $user->id }}">{{ $user->id }}</a></dd>
 
                 @if(! empty($user->parse_installation_ids))
                     <dt>Parse:</dt><dd><code>{{ implode(', ', $user->parse_installation_ids) }}</code></dd>
