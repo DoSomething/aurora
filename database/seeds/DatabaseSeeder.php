b51e860b33e68b5ae18bb94538c897c601f29d40
David Furnes <david@dfurnes.com>
2016-10-06 16:19:43 -0400

    Remove seeders.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,23 +1,16 @@
 <?php
 
+use Illuminate\Database\Seeder;
+
 class DatabaseSeeder extends Seeder
 {
     /**
-   * Run the database seeds.
-   *
-   * @return void
-   */
-  public function run()
-  {
-      Eloquent::unguard();
-      DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-
-      $this->call('UserTableSeeder');
-      $this->command->info('User table seeded');
-
-      $this->call('RolesTableSeeder');
-      $this->command->info('Roles table seeded');
-
-      DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-  }
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Nothing to seed.
+    }
 }
