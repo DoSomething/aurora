b7efa8dc954e9e1177232e32a2c89bdf8d03b2b2
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-10-23 17:42:50 -0400

    routes to preview and create user merges

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ $router->get('auth/logout', 'Auth\AuthController@getLogout');
 // Users
 $router->resource('users', 'UsersController', ['except' => ['create', 'store']]);
 $router->get('search', ['as' => 'user.search', 'uses' => 'UsersController@search']);
+$router->get('users/{user}/merge', ['as' => 'users.merge.create', 'uses' => 'MergeController@create']);
+$router->post('users/{user}/merge', ['as' => 'users.merge.store', 'uses' => 'MergeController@store']);
 
 // Superusers
 $router->resource('superusers', 'SuperusersController', ['only' => ['index']]);
