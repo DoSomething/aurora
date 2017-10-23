a5c928bcbac22599046cf17a3c18142eeadd54f3
Mendel Blesofsky <mendelblesofsky@gmail.com>
2017-10-23 17:42:31 -0400

    adding form to preview merging users

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -21,6 +21,20 @@
                         <div class="danger-zone">
                             <h4 class="danger-zone__heading">Danger Zone&#8482;</h4>
                             <div class="danger-zone__block">
+                                {!! Form::open(['route' => ['users.merge.create', $user->id], 'method' => 'GET']) !!}
+                                <div class="form-item">
+                                    <label for="role" class="field-label">Merge Account</label>
+                                    <p class="footnote">This will merge the account with given ID <strong>into this user's account</strong>. The user with the ID entered below will be deleted.</p>
+                                </div>
+                                <div class="form-item -padded">
+                                  {!! Form::label('id', 'ID', ['class' => 'field-label']) !!}
+                                  {!! Form::text('id', NULL, ['class' => 'text-field']) !!}
+                                </div>
+                                <div class="form-actions">
+                                    {!! Form::submit('Merge User', ['class' => 'button -secondary']) !!}
+                                </div>
+                                {!! Form::close() !!}
+
                                 {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'DELETE']) !!}
                                 <div class="form-item">
                                     <label for="role" class="field-label">Delete Account</label>
