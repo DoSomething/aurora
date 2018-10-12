94e2c48e08e4e537787f757dc61e0bd77b9346ae
David Furnes <david@dfurnes.com>
2018-10-12 17:48:23 -0400

    Add import & export command for Fastly data.

---
+++ app/Console/Commands/FastlyExportCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Aurora\Console\Commands;
+
+use Aurora\Services\Fastly;
+use Illuminate\Console\Command;
+
+class FastlyExportCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fastly:export';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Export Fastly dictionaries from this app\'s service.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(Fastly $fastly)
+    {
+        $redirects = $fastly->getAllRedirects();
+        $this->line(json_encode($redirects, JSON_PRETTY_PRINT));
+    }
+}
