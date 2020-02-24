ec4fbb5a204a6b9b43020c07a92edfb65f4df1f6
David Furnes <david@dfurnes.com>
2020-02-24 17:46:11 -0500

    Remove reference to Drupal ID.

--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -1,6 +1,6 @@
 {!! Form::open(['action' => 'UsersController@search', 'method' => 'GET']) !!}
 <div class="form-actions -inline">
-    <li>{!! Form::text('query', request()->get('query'), ['class' => 'text-field -search', 'placeholder' => 'Search by email, mobile, Drupal ID…', 'style' => 'min-width: 400px']) !!}</li>
+    <li>{!! Form::text('query', request()->get('query'), ['class' => 'text-field -search', 'placeholder' => 'Find user by email, mobile, ID…', 'style' => 'min-width: 400px']) !!}</li>
     <li>{!! Form::submit('Search', ['class' => 'button']) !!}</li>
 </div>
 {!! Form::close () !!}
