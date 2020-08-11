71aac4f4f3f5e71d7846b7c9906269b73a39c985
Diego Lorenzo <diego@diegolorenzo.com>
2020-08-11 14:55:05 -0400

    Updating miscellaneous.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,16 +9,6 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Schema::defaultStringLength(191);
-    }
-
-    /**
      * Register any application services.
      *
      * @return void
@@ -28,5 +18,15 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(Fastly::class, function ($app) {
             return new Fastly(config('services.fastly'));
         });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
     }
 }
