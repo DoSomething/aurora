8a64e7fe05b91dc24d67e23db42fa1641a546bf9
Diego Lorenzo <diego@diegolorenzo.com>
2020-08-12 01:50:42 -0400

    Additional updates.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'role' => \Aurora\Http\Middleware\CheckRole::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 
     /**
